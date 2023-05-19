6380f9a895e64233bc03c4d06c475c0e071d0838
Harshal-Aote <harshalaote11@gmail.com>
2023-05-19 18:46:09 +0530

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 
 import Add from "./component/Add/Add";
+import { useState } from "react";
 
 function App() {
+
+  const [inputValue, setInputValue] = useState()
+  // console.log('inputValue',inputValue)
+  // console.log('setInputValue',setInputValue)
+  const printValue = () => {
+    // console.log('inputValue printValue',inputValue)
+    return inputValue
+  }
+  // let inputValue = 5;
   return (
     <div>
       <h1>First React Project</h1>
       <p>This is p tag</p>
       <Add></Add>
+      <div>
+      <input
+       value={inputValue}
+       onChange={(e) => {
+ 
+        setInputValue(e.target.value)
+        // console.log('inputValue',inputValue);
+       }}
+        type='number' 
+         placeholder='Enter Number'/>
+    </div>
+    <button onClick={printValue}>print</button>
+   { inputValue ? <Add a={inputValue} b='2' ></Add>: ''} 
     </div>
   );
 }
--- src/component/Add/Add.js
+++ src/component/Add/Add.js
@@ -1,7 +1,9 @@
-const Add = () =>{
+const Add = (props) =>{
+    console.log('props',props)
     return(
         <div>
             <p>This is Add Component</p>
+            <p> Addition = {parseInt(props.a) + parseInt(props.b)} </p>
         </div>
     )
 }
