fb30321f5c5c3f23d22af5278845f728f4529a62
Harshal-Aote <harshalaote11@gmail.com>
2023-05-19 18:18:18 +0530

    My 1st Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Add from "./component/Add/Add";
+
+function App() {
+  return (
+    <div>
+      <h1>First React Project</h1>
+      <p>This is p tag</p>
+      <Add></Add>
+    </div>
+  );
+}
+
+export default App;
